403e84d95630e5ec4c1e8a21d69a4e3395e4b895
Pedro José NPS <pedrojosepaiva@outlook.com>
2023-11-16 09:52:34 -0300

    Aula 10: Eventos e callback

---
+++ aula-10/index.js
@@ -0,0 +1,33 @@
+/**
+ * Exercicio
+ * Utilizando a estrutura HTML/CSS abaixo, crie o script que irá fazer o botão mobile funcionar (ativar/desativar a navegação).
+ *
+ */
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(event) {
+    const button = event.currentTarget;
+    const nav = document.getElementById('nav');
+    if (button instanceof HTMLElement && nav) {
+        const active = nav.classList.contains('active');
+        if (active) {
+            nav.classList.remove('active');
+            button.setAttribute('aria-expanded', 'false');
+            button.setAttribute('aria-label', 'Abrir Menu');
+        }
+        else {
+            nav.classList.add('active');
+            button.setAttribute('aria-expanded', 'true');
+            button.setAttribute('aria-label', 'Fechar Menu');
+        }
+    }
+}
+btnMobile?.addEventListener('pointerdown', toggleMenu);
